8a7b697c523313cc57007343953297f79f999f33
Rihard4a <118355184+Rihard4a@users.noreply.github.com>
2023-05-20 10:58:17 +0300

    update

--- StartProjects.ps1
+++ StartProjects.ps1
@@ -1,19 +1,13 @@
-# Build the .NET backend
-Set-Location -Path "..\RoadStatApp\RoadStat"
-dotnet build --configuration Release
-
 # Navigate to the directory of your .NET app and start the process
 Set-Location -Path "..\RoadStatApp\RoadStat\bin\Release\net7.0"
 Start-Process -FilePath "RoadStat.exe"
 
-# Build the React frontend
-Set-Location -Path "..\RoadStatApp\web-ui"
-npm install
-npm run build
+# Go back to the root directory
+Set-Location -Path "..\..\..\.."
 
 # Start a new PowerShell process to run the frontend server
 Start-Process powershell -ArgumentList {
-    Set-Location -Path "../RoadStatApp/web-ui"
+    Set-Location -Path "..\RoadStatApp\web-ui"
     node server.js
 }
 
